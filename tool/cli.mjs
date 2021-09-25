90e51e566e648ff68410137bdb56c69927a03fd6
Willson Smith <me@willsonsmith.com>
2021-09-25 18:34:11 -0400

    i am a dumb

--- tool/cli.mjs
+++ tool/cli.mjs
@@ -8,10 +8,10 @@ const { source = 'src', target = 'dist', watch, build } = argv;
 (() => {
   if (build) {
     console.log('Building...');
-    return builder(source || 'src', target || 'dist');
+    return builder(source, target);
   }
   if (watch) {
     console.log('Watching...');
-    watcher(source || 'src', target || 'dist');
+    watcher(source, target);
   }
 })();
